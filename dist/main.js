77fcd4e82260decb11fc9d2de1b527915a616b1c
hari2058 <bhandarryharry@gmail.com>
2025-11-12 13:02:46 +0545

    todo created and id deleted

--- dist/main.js
+++ dist/main.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const http_1 = __importDefault(require("http"));
 const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
+let todos = [];
 const server = http_1.default.createServer(async (req, res) => {
     console.log("Request received", req.url);
     let body = "";
@@ -81,8 +82,96 @@ const server = http_1.default.createServer(async (req, res) => {
         console.log("response sent");
         res.end();
     }
-    // else if ( req.url == "/todo-route" && req.method === "POST") {
-    // }
+    else if (req.url == "/todo-route" && req.method === "POST") {
+        async function receivedData() {
+            return new Promise((resolve, reject) => {
+                req.on("data", (data) => {
+                    console.log("Todos Received", data.toString());
+                    body += data.toString();
+                    resolve(data);
+                });
+            });
+        }
+        await receivedData();
+        const bodyJSON = JSON.parse(body);
+        //Save the data
+        todos.push(bodyJSON);
+        res.writeHead(201, "created todos.", {
+            "Content-Type": "application/json",
+        });
+        res.write(JSON.stringify({
+            message: "Todos created.",
+            data: bodyJSON,
+        }));
+        console.log("todos created");
+        res.end();
+    }
+    else if (req.url === "/todo-route" && req.method === "GET") {
+        //get the data from the array
+        //send response
+        res.writeHead(200, "Todos Fetched", {
+            "content-type": "applicatiom/json",
+        });
+        res.write(JSON.stringify({
+            message: "todos fetched!",
+            data: todos,
+        }));
+        res.end();
+    }
+    else if (req.url?.includes("/todo-route?id=") && req.method === "DELETE") {
+        // "/todos?id=1"
+        // params = http://localhost:3000/todos/:id
+        //query http://localhost:3000/todos?id=1
+        const url = req.url;
+        console.log("delete a todo", url);
+        const idNullable = url.split("=")[1]; //['/todod?id', '1']
+        console.log("id nullable", idNullable);
+        if (!idNullable) {
+            res.writeHead(400, " Id not valid", {
+                "content-type": "application/json",
+            });
+            res.write(JSON.stringify({
+                message: "send the correct id!",
+            }));
+            res.end();
+            return;
+        }
+        const idNum = parseInt(idNullable);
+        //chech if todo exists in the array by the same id
+        const todoFound = todos.find((todo) => {
+            if (todo.id === idNum) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        });
+        if (!todoFound) {
+            res.writeHead(404, "Todo not found", {
+                "content-type": "application/json",
+            });
+            res.write(JSON.stringify({
+                message: "Todo not found!",
+            }));
+            res.end();
+        }
+        const updatedTodos = todos.filter((todo) => {
+            if (todo.id === idNum) {
+                return false;
+            }
+            else {
+                return true;
+            }
+        });
+        todos = updatedTodos;
+        res.writeHead(200, "Todo deleted", {
+            "content-type": "application/json",
+        });
+        res.write(JSON.stringify({
+            message: "Todo deketed successfully!",
+        }));
+        res.end();
+    }
     else {
         console.log("error  Page");
         res.writeHead(200, "error Page sent successfully.", {
@@ -100,6 +189,4 @@ const server = http_1.default.createServer(async (req, res) => {
 server.listen(3000, () => {
     console.log("started server @ http://localhost:3000");
 });
-//Homework
-//about-us page and 404 page
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,12 @@ import http from "http";
 import fs from "fs";
 import path from "path";
 
+interface Todo {
+  id: number;
+  name: string;
+  email: string;
+}
+let todos: Todo[] = [];
 const server = http.createServer(async (req, res) => {
   console.log("Request received", req.url);
 
@@ -101,11 +107,121 @@ const server = http.createServer(async (req, res) => {
 
     console.log("response sent");
     res.end();
-  }
-  // else if ( req.url == "/todo-route" && req.method === "POST") {
+  } else if (req.url == "/todo-route" && req.method === "POST") {
+    async function receivedData() {
+      return new Promise((resolve, reject) => {
+        req.on("data", (data) => {
+          console.log("Todos Received", data.toString());
+          body += data.toString();
+          resolve(data);
+        });
+      });
+    }
+    await receivedData();
+    const bodyJSON: Todo = JSON.parse(body);
 
-  // }
-  else {
+    //Save the data
+
+    todos.push(bodyJSON);
+
+    res.writeHead(201, "created todos.", {
+      "Content-Type": "application/json",
+    });
+
+    res.write(
+      JSON.stringify({
+        message: "Todos created.",
+        data: bodyJSON,
+      })
+    );
+    console.log("todos created");
+    res.end();
+  } else if (req.url === "/todo-route" && req.method === "GET") {
+    //get the data from the array
+
+    //send response
+
+    res.writeHead(200, "Todos Fetched", {
+      "content-type": "applicatiom/json",
+    });
+
+    res.write(
+      JSON.stringify({
+        message: "todos fetched!",
+        data: todos,
+      })
+    );
+    res.end();
+  } else if (req.url?.includes("/todo-route?id=") && req.method === "DELETE") {
+    // "/todos?id=1"
+    // params = http://localhost:3000/todos/:id
+    //query http://localhost:3000/todos?id=1
+
+    const url = req.url;
+    console.log("delete a todo", url);
+
+    const idNullable = url.split("=")[1]; //['/todod?id', '1']
+
+    console.log("id nullable", idNullable);
+
+    if (!idNullable) {
+      res.writeHead(400, " Id not valid", {
+        "content-type": "application/json",
+      });
+
+      res.write(
+        JSON.stringify({
+          message: "send the correct id!",
+        })
+      );
+      res.end();
+      return;
+    }
+
+    const idNum = parseInt(idNullable);
+
+    //chech if todo exists in the array by the same id
+
+    const todoFound = todos.find((todo) => {
+      if (todo.id === idNum) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+
+    if (!todoFound) {
+      res.writeHead(404, "Todo not found", {
+        "content-type": "application/json",
+      });
+
+      res.write(
+        JSON.stringify({
+          message: "Todo not found!",
+        })
+      );
+      res.end();
+    }
+
+    const updatedTodos = todos.filter((todo) => {
+      if (todo.id === idNum) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+
+    todos = updatedTodos;
+    res.writeHead(200, "Todo deleted", {
+      "content-type": "application/json",
+    });
+    res.write(
+      JSON.stringify({
+        message: "Todo deleted successfully!",
+      })
+    );
+    res.end();
+  } else {
     console.log("error  Page");
     res.writeHead(200, "error Page sent successfully.", {
       "content-Type": "text/html",
@@ -130,6 +246,3 @@ const server = http.createServer(async (req, res) => {
 server.listen(3000, () => {
   console.log("started server @ http://localhost:3000");
 });
-
-//Homework
-//about-us page and 404 page
