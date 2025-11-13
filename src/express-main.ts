669a9ab038c8210e60a3e0f51d7a47c8b9be7d3b
hari2058 <bhandarryharry@gmail.com>
2025-11-13 14:26:58 +0545

    Express todo creted

--- src/express-main.ts
+++ src/express-main.ts
@@ -2,20 +2,44 @@ import express from "express";
 
 const app = express();
 
+interface Todo {
+  id: number;
+  name: string;
+  email: string;
+}
+let todos: Todo[] = [];
+
+app.use(express.json());
+
 //routes
 
-app.get("/", (req, res) => {
-  res.send("Hello express updated");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello express updated");
+// });
 
-app.get("/contact-us", (req, res) => {
-  res.send(`<h1>contact Us</h1>`);
-});
+// app.get("/contact-us", (req, res) => {
+//   res.send(`<h1>contact Us</h1>`);
+// });
+
+
+//create todo route 
 
 app.post("/todo-route", (req, res) => {
+  console.log("body", req.body);
+  const newTodo = req.body as Todo;
+  todos.push(newTodo);
 
+  res.json({
+    message: "Created todo",
+     newTodo,
+  });
+});
 
-
+app.get("/todo-route", (req, res) => {
+  res.json({
+    message: "Received Todo.",
+   todos,
+  });
 });
 
 app.listen(4000, () => {
